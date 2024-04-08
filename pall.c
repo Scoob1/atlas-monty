b1a84fffd30078061ed562c6c5d80bd1f73001d5
Scoob1 <tamara_walling@yahoo.com>
2024-04-08 16:29:25 +0000

    pallc

--- pall.c
+++ pall.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include <stdio.h>
 
 /**
  * pall - Prints all the values on the stack
@@ -6,4 +7,12 @@
  * @line_number: Line number of the opcode in the file
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack)
+{
+	stack_t *current = *stack;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
