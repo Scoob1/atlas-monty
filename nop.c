222f998852bac06780cf02457b2dc7f0d7591693
Scoob1 <tamara_walling@yahoo.com>
2024-04-20 03:27:34 +0000

    had to fix nop

--- nop.c
+++ nop.c
@@ -6,7 +6,7 @@
  * @line_number: line number
  */
 
-void _nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)*stack;
 	(void)line_number;
