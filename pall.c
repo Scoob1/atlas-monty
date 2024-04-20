b780ffac6d4d81a9128f30f2515ce10757e32220
Scoob1 <tamara_walling@yahoo.com>
2024-04-20 02:41:01 +0000

    compile

--- pall.c
+++ pall.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 
 /**
- * pall - Prints all the values on the stack
- * @stack: Double pointer to the head of the stack
- * @line_number: Line number of the opcode in the file
- */
-
+ * pall- prints all the values on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number where instruction appears
+ * Return: Nothing
+*/
 void pall(stack_t **stack, unsigned int line_number)
 {
+	(void)line_number;
+
 	stack_t *current = *stack;
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 	}
-}
+}}
