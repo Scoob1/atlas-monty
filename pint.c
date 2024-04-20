9f571616c8ffa5b93204d674ea599fabe476aef1
Scoob1 <tamara_walling@yahoo.com>
2024-04-20 01:15:16 +0000

    header, pint, pop

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - prints value
+ * @stack: pointer tto the top of the stack
+ * @line_number: instructions appear
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
